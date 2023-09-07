8c613eb6888a8c39a44d9d693bcae79aebe0188c
11pplgb <11.pplg.b.b@gmail.com>
2023-09-07 11:40:42 +0700

    mengakses data base

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,3 +1,9 @@
+
+import os
+
+from cs50 import SQL
+from flask import import Flask,flask,jsonify,redirect,render_template,request,session
+
 #import apk flask untuk di pakai di web kita
 from flask import Flask
 #mangatur nama apk
